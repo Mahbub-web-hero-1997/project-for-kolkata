2d22187c5122817ae4dc617017df0e02753bc6c1
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2022-06-23 17:51:45 +0600

    responsive added

--- src/Components/Schedule/Schedule.js
+++ src/Components/Schedule/Schedule.js
@@ -18,23 +18,23 @@ const Schedule = () => {
 
     return (
         // Schedule Section
-        <section className='w-full md:w-11/12 mx-auto'>
-            <h1 className='text-3xl font-semibold text-left'>Schedule your pickup of eco-waste</h1>
+        <section className='w-full md:w-11/12 mx-auto p-2 lg:p-0'>
+            <h1 className='text-xl lg:text-3xl font-semibold text-center lg:text-left'>Schedule your pickup of eco-waste</h1>
             {/* First Section Start */}
-            <div className='w-full relative h-52 mt-20 mb-40'>
+            <div className='w-full relative h-screen lg:h-52 mt-8 lg:mt-20 mb-40'>
                 <div className='absolute top-[-5px] left-0 w-[35px] h-[35px] rounded-[50%] border-2 border-yellow-500 text-center flex justify-center items-center'>
                     <span>1</span>
                 </div>
                 <h6 className='font-semibold text-left mx-left ml-20 mt-16'>CHECK AVAILABILITY</h6>
-                <div className="form-control ml-20 mt-8 text-left">
-                    <label className="input-group mb-5">
+                <div className="form-control mt-8 text-center lg:text-left lg:ml-20">
+                    <label className="input-group mb-5 text-center">
                         <input type="text" placeholder="560025" className="input input-bordered" />
                         <button className='btn bg-lime-400 border-0 text-white'>Check Availability</button>
                     </label>
                     <span>Area <span className='text-green-400'>560025</span> serviceable add your address to continue</span>
                 </div>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12 ml-20 border-b-2 mb-52'>
-                    <div className='border-2 flex justify-between p-3 rounded-lg h-[170px]'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12 lg:ml-20 border-b-2 mb-52'>
+                    <div className='border-2 flex justify-center lg:justify-between p-3 rounded-lg h-[170px]'>
                         <div className='h-[20px]  flex items-center'>
                             <input className='w-[20px] h-[20px]  ' type="checkbox" name="" id="" />
                             <span className='ml-2'>Pickup from this address</span>
@@ -48,17 +48,16 @@ const Schedule = () => {
                 </div>
             </div>
             {/* First Section End */}
-
             {/* Second Section Start */}
             <div className='relative mt-[300px] mb-10'>
-                <div className='absolute top-[-5px] left-0 w-[35px] h-[35px] rounded-[50%] border-2 border-yellow-500 text-center flex justify-center items-center'>
+                <div className='absolute top-[-50px] lg:top-[-5px] left-[50%] lg:left-0 w-[35px] h-[35px] rounded-[50%] border-2 border-yellow-500 text-center flex justify-center items-center'>
                     <span>2</span>
                 </div>
-                <div className='grid grid-cols-3'>
-                    <h6 className='font-semibold text-left mx-left ml-20 '>Select what you have</h6>
-                    <h6 className='font-semibold text-left mx-left ml-20 '>Select what you have</h6>
+                <div className='grid grid-cols-1 lg:grid-cols-3'>
+                    <h6 className='font-semibold text-center lg:text-left lg:mx-left lg:ml-20 '>Select what you have</h6>
+                    <h6 className='font-semibold invisible lg:visible text-left mx-left ml-20 '>Select what you have</h6>
                 </div>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-5 ml-20 border-b-2'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:mt-5 lg:ml-20 border-b-2'>
                     {/* 2.1 section*/}
 
                     <div className='relative'>
@@ -117,22 +116,23 @@ const Schedule = () => {
                         </div>
                     </div>
                     {/* 2.3 Section */}
-                    <img src={img2} alt="" />
+                    <img className='mt-[-150px] lg:mt-0' src={img2} alt="" />
                 </div>
             </div>
             {/* Second Section End */}
+
             {/* Third Section Start */}
             <div className='relative mt-[50px] mb-10'>
-                <div className='absolute top-[-5px] left-0 w-[35px] h-[35px] rounded-[50%] border-2 border-yellow-500 text-center flex justify-center items-center'>
+                <div className='absolute top-[-5px] left-[50%] lg:left-0 w-[35px] h-[35px] rounded-[50%] border-2 border-yellow-500 text-center flex justify-center items-center'>
                     <span>3</span>
                 </div>
-                <div className='grid grid-cols-3'>
-                    <h6 className='font-semibold text-left mx-left ml-20 '>Select what you have</h6>
-                    <h6 className='font-semibold text-left mx-left ml-20 '>Select what you have</h6>
+                <div className='grid grid-cols-1 lg:grid-cols-3'>
+                    <h6 className='font-semibold text-center lg:text-left mx-center lg:mx-left ml-8 lg:ml-20 mt-10 lg:mt-0 '>Select what you have</h6>
+                    <h6 className='font-semibold invisible lg:visible text-center lg:text-left mx-left ml-20 '>Select what you have</h6>
                 </div>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-5 ml-20 border-b-2'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 lg:gap-6 mt-5 ml:ml-20 border-b-2'>
                     {/* 3.1 section*/}
-                    <div className=' pl-5'>
+                    <div className=' lg:pl-5 text-center w-full'>
                         <div class="form-control" >
                             <label class="input-group">
                                 <input type="text" placeholder="Pickup" class="input input-bordered w-[100%]" disabled />
@@ -148,7 +148,7 @@ const Schedule = () => {
                         />
 
                     </div>
-                    {/* 2.2 Section */}
+                    {/* 3.2 Section */}
                     <div className='border-l-2 h-[300px] pl-5'>
                         <p className=' text-left font-semibold'>Can you keep your paper outside the door a day before the schedule?</p>
                         <div className='flex mt-5'>
@@ -163,12 +163,11 @@ const Schedule = () => {
                         </div>
                         <button className=' btn bg-green-500 mt-10 border-0 w-full text-white font-bold'>Place Request</button>
                     </div>
-                    {/* 2.3 Section */}
-                    <img src={img3} alt="" />
+                    {/* 3.3 Section */}
+                    <img className='mt-[-110px] lg:mt-0' src={img3} alt="" />
                 </div>
             </div>
             {/* Third Section End */}
-
         </section >
     );
 };
